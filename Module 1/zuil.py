49886b3709f4a0951675dd8a377474edb6984633
Chazzvdh <chazz.vandenhof@student.hu.nl>
2022-10-07 12:49:27 +0200

    v0.01

--- Module 1/zuil.py
+++ Module 1/zuil.py
@@ -7,7 +7,18 @@ station = random.choice(stations)
 
 naam = input("Wat is uw naam?: ")
 current_time = datetime.datetime.now()
-bericht = input("Voer hier uw bericht in met een maximale va 140 tekens: ")
+
+while True:
+    bericht = input("Voer hier uw bericht in (maximaal 140 tekens): ")
+    if len(bericht) > 140:
+        print("Het bericht mag niet langer zijn dan 140 tekens.")
+        print("Uw bericht was {} tekens lang.\nprobeer het opnieuw:".format(len(bericht)))
+        continue
+    else:
+        break
+
+if naam == "":
+    naam = "Anoniem"
 
 file1.write("Naam gebruiker: {}\nDatum en tijd {}\nBericht: {}\nStation: {}\n\n".format(naam, current_time, bericht, station))
 file1.close()
