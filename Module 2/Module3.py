78483c52fec433bc223ba0be81f7b1c93e103c71
Chazzvdh <chazz.vandenhof@student.hu.nl>
2022-11-02 19:33:01 +0100

    v0.05
    Done:
    Module 1 af
    module 2 af
    
    Doing:
    Module 3 GUI grotendeels af alleen nog data importen uit het database.
    
    To do:
    Module 1 GUI
    Module 2 GUI
    Module 3 GUI nog mooier maken

--- Module 2/Module3.py
+++ Module 2/Module3.py
@@ -4,8 +4,11 @@ from tkinter.font import Font
 import requests
 from PIL import Image, ImageTk
 
-stations = ["Amsterdam", "Haarlem", "Breda"]
-station = random.choice(stations)
+def clicked():
+    l1.configure(text=f"Station: {option.get()}\nTemperature: {getData(option.get())[4]}°C\nDescription: {getData(option.get())[3]}")
+    window.update()
+    print(f"Refresh weather data voor: '{option.get()}'")
+    return
 
 def getData(station):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=ca6a55a3a2834c3f154a90676cb9b565&units=metric&lang=nl'.format(station)
@@ -43,11 +46,19 @@ my_font2 = Font(
     overstrike = False
 )
 
-list_val = StringVar(window)
-list_val.set("Station")
-option_menu = OptionMenu(window, list_val, "Amsterdam", "Haarlem", "Breda")
-option_menu.configure(font=my_font2, activebackground="grey40", activeforeground="grey100", bg="grey25", fg="gray60")
-option_menu.grid(row=0, column=0, ipady=10, ipadx=10, sticky=W)
+label2 = Label(window)
+label2.configure(bg="grey18")
+label2.grid(row=0, column=0, rowspan=1, columnspan=4, sticky=NSEW)
+
+option = StringVar(window)
+option.set("Amsterdam")
+option_menu = OptionMenu(window, option, "Amsterdam", "Haarlem", "Breda")
+option_menu.configure(font=my_font2, activebackground="grey40", activeforeground="grey100", bg="grey25", fg="gray100")
+option_menu.grid(row=0, column=0, rowspan=1, columnspan=1, ipady=5, ipadx=10, sticky=W)
+
+button1 = Button(window, command=clicked)
+button1.configure(text="Refresh", font=my_font2, activebackground="grey40", activeforeground="grey100", bg="grey25", fg="gray100")
+button1.grid(row=0, column=3, rowspan=1, columnspan=1, ipady=5, ipadx=10, sticky=E)
 
 image = Image.open("ns-logo2.png")
 resize_image = image.resize((100, 50))
@@ -56,9 +67,9 @@ label1 = Label(master=window, image=img)
 label1.configure(bg="gray20")
 label1.grid(row=1, column=3, rowspan=1, columnspan=1, ipadx=20, ipady=20, sticky=E)
 
-l1 = Label(window, justify=LEFT, text=f"Station: {station}\nTemperature: {getData(station)[4]}°C\nDescription: {getData(station)[3]}")
+l1 = Label(window, justify=LEFT, text=f"Station: {option.get()}\nTemperature: {getData(option.get())[4]}°C\nDescription: {getData(option.get())[3]}")
 l1.configure(bg="gray20", fg="gray80", font=my_font1, anchor="center")
-l1.grid(row=1, column=0, rowspan=1, columnspan=1, ipadx=20, ipady=20, sticky=W)
+l1.grid(row=1, column=0, rowspan=1, columnspan=1, ipadx=10, ipady=10, sticky=W)
 
 text1 = Text(window)
 text1.configure(font=my_font2, fg="white", bg="grey18", height=6)
